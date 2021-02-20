a8ba8d82ec5dec8a9b203b4d425d0c16ccbadd35
DevenMarrero <70239160+DevenMarrero@users.noreply.github.com>
2021-02-20 12:36:22 -0800

    Split classes into seperate files

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,19 +1,58 @@
 #include "Tetris.hpp"
 
-// Tetris
+// Tetris Class
 Tetris::Tetris()
 {}
 Tetris ::~Tetris()
 {}
 
-void Tetris::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+void Tetris::init(const char* title, int xpos, int ypos, int width, int height)
 {
+	isRunning = false;
+	// Initialize SDL library
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		cout << "SDL Initialized\n";
+		// Create the window to draw on
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, 0);
+		if (window) {
+			cout << "Window Created\n";
+
+			// Create the renderer to draw on the window
+			renderer = SDL_CreateRenderer(window, -1, 0);
+			if (renderer) {
+				cout << "Renderer Created\n";
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				isRunning = true;
+			}
+			else {
+				cout << "Error Initializing Renderer\n";
+			}
+		}
+		else {
+			cout << "Error Initializing Window\n";
+		}
+	}
+	else{
+		cout << "Error Initializing SDL\n";
+	}
 
 }
 
 void Tetris::handleEvents()
 {
+	// Check for some kind of event
+	SDL_Event event;
+	SDL_PollEvent(&event);
 
+	switch (event.type){
+	// Exit button pressed
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+
+	default:
+		break;
+	}
 }
 
 void Tetris::update()
@@ -21,33 +60,21 @@ void Tetris::update()
 
 }
 
+void Tetris::render()
+{
+	SDL_RenderClear(renderer);
+	// Render objects here
+
+	// Set background to black and show new frame
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderPresent(renderer);
+}
+
 void Tetris::clean()
 {
-
-}
-
-
-
-// Figure
-Figure::Figure(int xpos, int ypos)
-{
-	x = xpos;
-	y = ypos;
-	// Set random seed
-	srand(time(NULL));
-	// Pick random figure index
-	type = rand() % (figures.size() -1);
-	srand(time(NULL));
-	colour = rand() % (colours.size() -1);
-	rotation = 0;
-}
-
-vector<int> Figure::state()
-{
-	return figures[type][rotation];
-}
-
-void Figure::rotate()
-{
-	rotation = (rotation + 1) % figures[type].size();
+	// Clear memory used by SDL
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	cout << "Game Cleaned\n";
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 //
 
 #include "Tetris.hpp"
+#include "Figure.hpp"
 #include "SDL.h"
 using namespace std;
 
+Tetris* tetris = nullptr;
+
 int main(int argc, char *argv[])
 {
+
+    tetris = new Tetris();
+
+    tetris->init("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 600);
+
+    while (tetris->running()) {
+        tetris->handleEvents();
+        tetris->update();
+        tetris->render();
+    }
+
+    tetris->clean();
 
     return 0;
 }
